2576aac3a3ab1d511834b057efae06e0215017bf
unknown <chichelezcano94@hotmail.com.ar>
2023-07-05 11:36:57 -0300

    mas cambios

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const path = require('path');
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
+const { sequelize } = require("./database");
 
-const { sequelize } = require('./database');
-
-sequelize.authenticate()
-    .then(() => console.log('Conexión a base de datos exitosa'))
-    .catch((error) => console.log('Error al conectar a base de datos', error));
-
+sequelize
+  .authenticate()
+  .then(() => console.log("Conexión a base de datos exitosa"))
+  .catch((error) => console.log("Error al conectar a base de datos", error));
 
 const app = express();
 
-require('ejs');
+require("ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Configuración del motor de plantillas
+app.set("views", path.join(__dirname, "views")); // Corrección: Se corrige la sintaxis del método 'join'
+app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 const port = process.env.PORT || 5000;
-
 
 // Middlewares
 // TODO: Implementar middlewares
 app.use(cors());
 //app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 // Routes
-app.use(require('./routes/reserva.routes'));
+app.use(require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
 
 app.use((req, res, next) => {
-    res.write(`<div>
+  res.write(`<div>
         <h1>404 - Ruta no encontrada</h1>
         <hr>
         <p>La pagina que intentas buscar no existe</p>
@@ -59,7 +57,6 @@ app.use((req, res, next) => {
         </script>
     </h1>`);
 });
-
 
 // Starting the server
 app.listen(port, console.log(`Servidor corriendo en http://localhost:${port}`));
---
+++ reservadb1.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-07-2023 a las 16:31:48
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `reservadb1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` tinyint(1) DEFAULT 1,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `deletedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
